604fbd565b9255b63295e66ff0110fcfdc39af42
Zekerijah Halilovic <zhalilovic02@gmail.com>
2022-12-21 16:35:04 -0500

    commit tree 5db01ccd77676906345a37ddcb13fda4121852ec

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+
+const names = ["Aki, Guadalupe, Ollie"];
+function writeCards(names, event) {
+    let messages =[...names]
+    for (let i =0; i < messages.length; i++) {
+        console.log(`Thank you, ${names}, for the wonderful suprise gift!`)
+        return messages;
+    }
+}
+  
+
+
+
